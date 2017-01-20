917aa71f00cd485810ee64d1116c04e060210a63
Seungjae Moon <sjaymoon15@gmail.com>
2017-01-20 17:58:51 -0500

    initialize firebase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD0tV5m8gaZsTY0-Wqao56AISAYTRiMy8U',
+      authDomain: 'authentication-77dc1.firebaseapp.com',
+      databaseURL: 'https://authentication-77dc1.firebaseio.com',
+      storageBucket: 'authentication-77dc1.appspot.com',
+      messagingSenderId: '355798434262'
+    });
+  }
   render() {
     return (
       <View>
